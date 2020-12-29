591d052fc4d55a66b454b8071a4cf35f876666ac
C3nsored <ur.gabriel@outlook.com>
2020-12-29 00:54:24 +0000

    commments base_enum.py

--- weatherterm/core/base_enum.py
+++ weatherterm/core/base_enum.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 # Base Enum class should used in other Enum classes 
- 
 class BaseEnum(Enum):
     def _generate_next_value_(name, start, count, last_value):
         return name 
